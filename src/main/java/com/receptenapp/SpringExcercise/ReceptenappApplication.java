a1b964cb72703ddff0506cff86604bc6ba333bb2
Alexandernevsk <Alexandertorreman@gmail.com>
2023-08-28 10:22:38 +0200

    Set-up project

---
+++ src/main/java/com/receptenapp/SpringExcercise/ReceptenappApplication.java
@@ -0,0 +1,13 @@
+package com.receptenapp.SpringExcercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptenappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceptenappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/receptenapp/SpringExcercise/ReceptenappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.receptenapp.SpringExcercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceptenappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
